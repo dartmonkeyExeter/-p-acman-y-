2937a9a42c81f3ba46e5025c9a33d09736aecea4
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2024-03-10 11:46:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # pac-man
 
-import pygame, math
+import pygame, math, random
 
 pygame.init()
 clock = pygame.time.Clock()
 fps = 75
 size = [560, 640]
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("pacman!!!!")
 
 class Grid:
     def __init__(self):
@@ -116,6 +117,8 @@ class Pacman:
         self.animation_frame = 0
         self.animate_backwards = False
         self.moving = True
+        self.power_pellet_clock = 0
+        self.score = 0
 
     def draw(self):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
@@ -175,13 +178,44 @@ class Pacman:
             else:
                 self.moving = True
 
-    def eat_check(self):
+    def eat_check(self, ghost_list):
         current_array = list(grid.grid[int(self.y / 20)])  # Convert string to list
+        ghost_positions = [(int(ghost.x / 20), int(ghost.y / 20)) for ghost in ghost_list]
         if current_array[int(self.x / 20)] == ".":
             current_array[int(self.x / 20)] = " "
+            self.score += 10
         if current_array[int(self.x / 20)] == "*":
             current_array[int(self.x / 20)] = " "
-        grid.grid[int(self.y / 20)] = "".join(current_array)  # Convert list back to string
+            for ghost in ghost_list:
+                ghost.edible = True
+            self.power_pellet_clock = fps * 10
+            self.score += 50
+        
+        grid.grid[int(self.y / 20)] = "".join(current_array)
+
+        if ghost_positions.count((int(self.x / 20), int(self.y / 20))) > 0:
+            if not ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].edible:
+                self.x = 30
+                self.y = 30
+                self.score -= 100
+                for ghost in ghost_list:
+                    ghost.x = ghost.start_pos[0] * 20
+                    ghost.y = ghost.start_pos[1] * 20
+                    ghost.edible = False
+                    ghost.path = None
+            elif ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].edible:
+                ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].x = ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].start_pos[0] * 20
+                ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].y = ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].start_pos[1] * 20
+                ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].edible = False
+                ghost_list[ghost_positions.index((int(self.x / 20), int(self.y / 20)))].path = None
+                self.score += 200
+
+    def power_pellet_timer(self):
+        if self.power_pellet_clock > 0:
+            self.power_pellet_clock -= 1
+        else:
+            for ghost in ghosts:
+                ghost.edible = False
 
     def animation(self):
         if self.moving:
@@ -198,7 +232,8 @@ class Pacman:
         self.draw()
         self.move()
         self.clamping()
-        self.eat_check()
+        self.eat_check(ghosts)
+        self.power_pellet_timer()
         self.animation()
 
 class Ghost:
@@ -212,49 +247,68 @@ class Ghost:
         self.calculate_path = True
         self.path = []
         self.teleport_timer = 0
+        self.edible = False
+        self.start_pos = (int(x / 20), int(y / 20))
 
     def draw(self):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.radius * 2, self.radius * 2))
-
+        if self.edible:
+            pygame.draw.rect(screen, (0,0,255), (self.x, self.y, self.radius * 2, self.radius * 2))
+        else: 
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.radius * 2, self.radius * 2))
+    
     def BFS(self, start, end, grid):
-        queue = [[start]]
-        visited = set()
-        while queue:
-            path = queue.pop(0)
-            node = path[-1]
-            if node == end:
-                return path
-            elif node not in visited:
-                visited.add(node)
-                for adjacent in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                    new_x = node[0] + adjacent[0]
-                    new_y = node[1] + adjacent[1]
-                    if (0 <= new_x < len(grid[0])) and (0 <= new_y < len(grid)) and grid[new_y][new_x] != "#":
-                        new_path = list(path)
-                        new_path.append((new_x, new_y))  # Fixed the order here
-                        queue.append(new_path)  # Enqueue the new path
-        # If no path is found
-        return None
-
-
+        queue = {start: [[start]]}
+        visited = {start: set()}
+        all_paths = []
+        while any(queue.values()):
+            for ghost, path_queue in queue.copy().items():
+                path = path_queue.pop(0)
+                node = path[-1]
+                if node == end:
+                    all_paths.append(path)
+                elif node not in visited[ghost]:
+                    visited[ghost].add(node)
+                    random.shuffle([(0, -1), (0, 1), (-1, 0), (1, 0)])
+                    for adjacent in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                        new_x = node[0] + adjacent[0]
+                        new_y = node[1] + adjacent[1]
+                        if (0 <= new_x < len(grid[0])) and (0 <= new_y < len(grid)) and grid[new_y][new_x] != "#":
+                            new_path = list(path)
+                            new_path.append((new_x, new_y))
+                            if ghost not in queue:
+                                queue[ghost] = []
+                            queue[ghost].append(new_path)
+        return all_paths
 
     def move(self):
-        # for now we're just gonna teleport the ghost to the next node in the path every second cause I'm lazy
         if self.path and self.teleport_timer == 0:
             next_node = self.path.pop(0)
             self.x = next_node[0] * 20
             self.y = next_node[1] * 20
     
     def update(self):
-        if self.calculate_path:
+        if self.calculate_path and not self.edible:
             pacman_array = (int(pacman.x / 20), int(pacman.y / 20))
             ghost_array = (int(self.x / 20), int(self.y / 20))
-            self.path = self.BFS(ghost_array, pacman_array, grid.grid)
+            self.path = random.choice(self.BFS(ghost_array, pacman_array, grid.grid))
             self.calculate_path = False
-            print(self.path)
+        
+        elif self.calculate_path and self.edible:
+            pacman_array = (int(pacman.x / 20), int(pacman.y / 20))
+            ghost_array = (int(self.x / 20), int(self.y / 20))
+            self.path = random.choice(self.BFS(ghost_array, self.start_pos, grid.grid))
+            self.calculate_path = False
+        
         self.draw()
         self.move()
-        
+        self.behaviours()
+
+    def behaviours(self):
+        if random.randint(0, 100) == 0:
+            self.calculate_path = True
+        if self.path is None:
+            self.calculate_path = True
+
 
 
 pacman = Pacman()
@@ -277,7 +331,7 @@ while running:
     for ghost in ghosts:
         ghost.update()
         ghost.teleport_timer += 1
-        if ghost.teleport_timer == 30:
+        if ghost.teleport_timer == 15:
             ghost.teleport_timer = 0
     grid.draw()
 
